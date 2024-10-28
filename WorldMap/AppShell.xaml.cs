4a0bb353440acacf5372acd344867fcc067ee785
Dober09 <lebohanglehutjo@gmail.com>
2024-10-28 15:50:43 +0200

    Add project files.

---
+++ WorldMap/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WorldMap
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WorldMap/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WorldMap
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

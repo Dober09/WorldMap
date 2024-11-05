0166978e641e5115558bbad53abca888e2f5feb2
Dober09 <lebohanglehutjo@gmail.com>
2024-11-06 01:47:38 +0200

    modified the landing page

--- WorldMap/View/LandingPage.xaml.cs
+++ WorldMap/View/LandingPage.xaml.cs
@@ -1,4 +1,6 @@
 using WorldMap.ViewModel;
+using WorldMap.Services;
+using System.Xml.Linq;
 namespace WorldMap.View;
 
 public partial class LandingPage : ContentPage
@@ -6,7 +8,10 @@ public partial class LandingPage : ContentPage
 
     public LandingPage()
     {
-        InitializeComponent();       
+        InitializeComponent();    
+        
+        var vm = new CountryViewModel(new DataServices());
+        BindingContext = vm;
     }
   
     
